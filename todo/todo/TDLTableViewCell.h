cc864df17458a9c4f8bc95d83474403f87a4b795
jdmgithub <jdm_email@yahoo.com>
2014-05-06 16:03:04 -0400

    todo revamped

---
+++ todo/todo/TDLTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  TDLTableViewCell.h
+//  todo
+//
+//  Created by Jeffery Moulds on 4/3/14.
+//  Copyright (c) 2014 Jeffery Moulds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLTableViewCell : UITableViewCell
+
+@property (nonatomic) NSInteger index;
+
+@end
